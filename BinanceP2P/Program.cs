181692642c6f13b69df50e391971e769966ff324
Beka Pukhashvili <beka.pukhashvili@gmail.com>
2022-05-01 00:14:12 +0400

    Major Bug Fix

--- BinanceP2P/Program.cs
+++ BinanceP2P/Program.cs
@@ -1,12 +1,11 @@
 ﻿using BinanceP2P;
 
-var ReceivedData = await Helper.GetData(
-  BaseUrl: "https://c2c.binance.com/", 
-  QueryString: "bapi/c2c/v2/friendly/c2c/adv/search"
-);
-
 while (true)
 {
+  var ReceivedData = await Helper.GetData(
+    BaseUrl: "https://c2c.binance.com/",
+    QueryString: "bapi/c2c/v2/friendly/c2c/adv/search"
+  );
   Helper.PrintResults(ReceivedData);
   await Task.Delay(60000);
 }
